25ac79acc94dfcd5a2e03d1462ca5debbf9b230c
HarveyMendez <harvey.mendez@ucr.ac.cr>
2024-06-12 21:46:34 -0600

    fixing errors

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include 'business/EmpresaManager.php';
-include 'business/CuponManager.php';
-require '../data/Database.php';
+include 'business/CouponManager.php';
+include 'data/Database.php';
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
@@ -379,64 +379,6 @@ if (strpos($_SERVER['REQUEST_URI'], '/index.php/getCoupon') !== false && $_SERVE
 
 // Manejar solicitud para obtener cupones de usuario
 if (strpos($_SERVER['REQUEST_URI'], '/index.php/getUserCoupon') !== false && $_SERVER['REQUEST_METHOD'] == 'GET') {
-<<<<<<< HEAD
-=======
-    
-        
-
-
-        if (isset($_GET['id'])) {
-            $id = $_GET['id'];
-            $query = "SELECT 
-                        c.id,
-                        e.nombre_empresa,
-                        e.ubicacion AS ubicacion_empresa,
-                        c.nombre AS nombre_cupon,
-                        c.precio,
-                        c.codigo,
-                        c.descuento,
-                        c.fecha_vencimiento,
-                        cat.nombreCategoria AS categoria,
-                        c.image
-                    FROM 
-                        Empresa e
-                    JOIN 
-                        Cupones c ON e.nombre_usuario = c.usuarioEmpresa
-                    JOIN 
-                        Categorias cat ON c.categoria = cat.id
-                        WHERE c.estado=true and c.id = '$id'";
-        $resultado = metodoGet($query);
-        echo json_encode($resultado->fetchAll());
-        } else {
-            $query = "SELECT 
-                        c.id,
-                        e.nombre_empresa,
-                        e.ubicacion AS ubicacion_empresa,
-                        c.nombre AS nombre_cupon,
-                        c.precio,
-                        c.codigo,
-                        c.descuento,
-                        c.fecha_vencimiento,
-                        cat.nombreCategoria AS categoria,
-                        c.image
-                    FROM 
-                        Empresa e
-                    JOIN 
-                        Cupones c ON e.nombre_usuario = c.usuarioEmpresa
-                    JOIN 
-                        Categorias cat ON c.categoria = cat.id
-                        WHERE c.estado=true";
-        $resultado = metodoGet($query);
-        echo json_encode($resultado->fetchAll());
-    
-        }
-        exit();
-
-}
-
-if (strpos($_SERVER['REQUEST_URI'], '/index.php/getCategories') !== false && $_SERVER['REQUEST_METHOD'] == 'GET') {
-    
->>>>>>> parent of 89b9eaa (add columna cantidad in return method getUserCoupon)
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
         $query = "SELECT 
